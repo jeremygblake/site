ab68762c83c4c6e6c03675a730b46f9dadcd3600
Jeremy <jeremygblake@users.noreply.github.com>
2020-02-13 17:29:03 -0800

    +

---
+++ src/components/HomePage.component.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Fade from 'react-reveal/Bounce'
+import './styles/homepage.css';
+
+export default class HomePage extends Component
+{
+  render()
+  {
+    const homepage =
+    {
+      
+    }
+    return (
+     <div className="homepage">
+       
+     </div>
+    )
+  }
+}
