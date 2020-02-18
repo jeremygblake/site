534bbc75a92a37414a60a1531fd6a702b1140832
Jeremy <32287708+jeremygblake@users.noreply.github.com>
2020-02-17 17:44:08 -0800

    +

---
+++ src/components/snippet.component.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './styles/snippet.css';
+
+export default class Snippet extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <title>Home Page</title>
+                <h1>
+                    WEBSITE
+                </h1>
+            </div>
+        )
+    }
+}
